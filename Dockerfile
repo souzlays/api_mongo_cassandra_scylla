7f5e79d67f06d7c054fcb9faf77f80aa654df194
douglas <estag.douglassales@pge.pr.gov.br>
2024-04-05 17:45:07 -0300

    criação da api em fastapi e renderização do docs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
-FROM python:3.6 as dev
+FROM python:3.9 as dev
 
-ADD . /app
+WORKDIR /code
 
-WORKDIR /app
+COPY ./requirements.txt /code/requirements.txt
 
-RUN pip install -r requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+
+EXPOSE 80 
+
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80", "--reload"]
--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify
+from fastapi import FastAPI
 import pymongo
 from pymongo import MongoClient
+import uvicorn
+from fastapi.responses import JSONResponse
+from fastapi.openapi.utils import get_openapi
+from fastapi.middleware.cors import CORSMiddleware
 
-app = Flask(__name__)
 
-app.debug = True
+
+app = FastAPI()
 
 def get_db():
     client = MongoClient(host='test_mongodb',
@@ -15,16 +19,38 @@ def get_db():
     db = client["pokedex_db"]
     return db
 
-@app.route('/')
-def ping_server():
+@app.get("/", tags=["Saudação"])
+async def read_root():
     return "Bem-vindo a pokedex NOSQL!"
 
-@app.route('/pokemons-mongodb')
-def get_stored_pokemon():
+@app.get('/pokemons-mongodb', tags=["Pokemons"])
+async def get_stored_pokemon():
     db = get_db()
     _pokemons = db.pokemon_tb.find()
     pokemons = [{"id": pokemon["id"], "name": pokemon["name"], "type": pokemon["type"]} for pokemon in _pokemons]
-    return jsonify({"pokemons": pokemons})
+    return JSONResponse(content={"pokemons": pokemons})
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Pokemon",
+        version="1.0.0",
+        description="Pokemons",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="10.38.24.35", port=5000, reload=True)
