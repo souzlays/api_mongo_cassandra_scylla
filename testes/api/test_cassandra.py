b30beccbb72981f118cbd5d3370b0d14adc24eb2
douglas <estag.douglassales@pge.pr.gov.br>
2024-05-09 16:01:56 -0300

    operação get testada e finalizada.

--- testes/api/test_cassandra.py
+++ testes/api/test_cassandra.py
@@ -4,20 +4,49 @@ import json
 
 
 class Cassandradb_teste(unittest.TestCase): 
-
-    def test_get_all_pokemon(self):
-        with open("pokemons_cassandra.json", "r", encoding="utf-8") as file:
-            expected_data = json.load(file)
-
+    
+    def test_get_capturando_primeiro_documento(self):
         response = requests.get("http://localhost:5000/pokemons-cassandra")
         self.assertEqual(response.status_code, 200)
-        pokemon_data = response.json()
+        documento_esperado = [
+            {
+                "name": "pikachu",
+                "type": "elétrico"
+            },
+            {
+                "name": "charizard",
+                "type": "fogo"
+            },
+            {
+                "name": "dug",
+                "type": "terra"
+            },
+            {
+                "name": "squirtle",
+                "type": "água"
+            }
+        ]
+        self.assertEqual(response.json(), documento_esperado)
+    
+    
+    
+    
+    
 
-        self.assertEqual(len(pokemon_data), len(expected_data))
+    # def test_get_all_pokemon(self):
+    #     with open("pokemons_cassandra.json", "r", encoding="utf-8") as file:
+    #         expected_data = json.load(file)
 
-        for i, pokemon in enumerate(pokemon_data):
-            self.assertEqual(pokemon["name"], expected_data[i]["name"])
-            self.assertEqual(pokemon["type"], expected_data[i]["type"])
+    #     response = requests.get("http://localhost:5000/pokemons-cassandra")
+    #     self.assertEqual(response.status_code, 200)
+    #     pokemon_data = response.json()
+
+    #     self.assertEqual(len(pokemon_data), len(expected_data))
+
+    #     for i, pokemon in enumerate(pokemon_data):
+    #         self.assertEqual(pokemon["name"], expected_data[i]["name"])
+    #         self.assertEqual(pokemon["type"], expected_data[i]["type"])
+    #         self.assertEqual(pokemon["ordem"], expected_data[i]["ordem"])
             
 
 if __name__ == '__main__':
