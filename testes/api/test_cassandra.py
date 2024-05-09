ecf777c09e621cec31829b4570bc835a18f4bb70
douglas <estag.douglassales@pge.pr.gov.br>
2024-05-09 16:02:43 -0300

    operação get testada e finalizada.

--- testes/api/test_cassandra.py
+++ testes/api/test_cassandra.py
@@ -27,27 +27,7 @@ class Cassandradb_teste(unittest.TestCase):
             }
         ]
         self.assertEqual(response.json(), documento_esperado)
-    
-    
-    
-    
-    
-
-    # def test_get_all_pokemon(self):
-    #     with open("pokemons_cassandra.json", "r", encoding="utf-8") as file:
-    #         expected_data = json.load(file)
-
-    #     response = requests.get("http://localhost:5000/pokemons-cassandra")
-    #     self.assertEqual(response.status_code, 200)
-    #     pokemon_data = response.json()
-
-    #     self.assertEqual(len(pokemon_data), len(expected_data))
-
-    #     for i, pokemon in enumerate(pokemon_data):
-    #         self.assertEqual(pokemon["name"], expected_data[i]["name"])
-    #         self.assertEqual(pokemon["type"], expected_data[i]["type"])
-    #         self.assertEqual(pokemon["ordem"], expected_data[i]["ordem"])
-            
+   
 
 if __name__ == '__main__':
     unittest.main()    
